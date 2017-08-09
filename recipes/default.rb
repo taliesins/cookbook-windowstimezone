a75c4cb85b7b4682dca47df0f5514e6469ebc75f
Taliesin Sisson <taliesins@interxion.com>
2017-08-09 09:29:08 +0100

    Need to escape backslash

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,14 @@
 powershell_script 'copy_certificates_to_chef_certificates_store' do		
 	code <<-EOH
+		$ErrorActionPreference = "Stop"
         $TimeZoneId = "#{node['windowstimezone']['timezoneid']}"
-        $tzutilPath = join-path $env:windir 'system32\tzutil'
+        $tzutilPath = Join-Path -Path $env:windir -ChildPath 'system32\\tzutil.exe'
         $CurrentTimeZoneId = &$tzutilPath /s $TimeZoneId
 	EOH
 	only_if <<-EOH2
+		$ErrorActionPreference = "Stop"
         $TimeZoneId = "#{node['windowstimezone']['timezoneid']}"
-        $tzutilPath = join-path $env:windir 'system32\tzutil'
+        $tzutilPath = Join-Path $env:windir 'system32\\tzutil.exe'
         $CurrentTimeZoneId = &$tzutilPath /g
         $TimeZoneMustBeChanged = $TimeZoneId -ne $CurrentTimeZoneId
 		
